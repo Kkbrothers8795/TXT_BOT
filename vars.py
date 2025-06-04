922b976d9c384663bd5a94c9299b01d3faa1a8ab
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-06-04 20:10:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,10 @@ API_HASH ="e743e5a4f35076e4c558a4bd713082e9"
 BOT_TOKEN = "7907920004:AAG18EpyWZKX27apayhahGDsQ8tB3NqBW-0"
 OWNER = "7788212890"
 CREDIT = "kIRODWAL"
-AUTH_USER = '7788212890'
-AUTH_USERS = '7788212890'
+AUTH_USER = "7788212890"
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
